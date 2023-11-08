9b90012c1bc6abd3c944a78db40107d5341ed2ac
kdr <132037908+khadherinc@users.noreply.github.com>
2023-11-07 23:30:28 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,111 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+
+//
+global.available = true;
+global.autoReadGc = false;
+global.autoReadAll = false;
+global.antitags = true;
+
+
+//auto functioner
+global.autoTyping = false;                //auto tying by default off.
+global.autoRecord = false;                //auto recording by default off.
+global.groupevent = false;                //This is the new variable for controlling group event handling.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
+
+
+//
+global.Owner = ["917560920774"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["917560920774"];     //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["917560920774"];  
+global.OwnerName = "KHADHER";
+global.BotName = "ELLIE";
+global.packname = "ELLIE";                             //Do not change.
+global.author = "By: KHADHER";                               //Do not change.
+global.BotSourceCode = "https://github.com/khadherinc/ELLIE"; //Do not change.
+global.SupportGroupLink = "https://chat.whatsapp.com/C4ivwZKuh5bLJkqfYNPQsk"; 
+
+
+//
+global.sessionName = "session";                          //Do not change.
+
+
+//
+global.prefa = ["."];                                    //Default prefix here.
+global.openAiAPI = "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";
+
+
+//
+global.location = "KANNUR, KERALA, INDIA";                   
+global.reactmoji = "";
+global.themeemoji = "";
+global.vidmenu = { url: 'https://tenor.com/view/jujutsu-kaisen0-yuta-okkotsu-gif-26767662' };
+global.websitex = "https://github.com/";
+global.lolhuman = "KHADHER";
+
+
+//
+global.BotLogo = fs.readFileSync("./Assets/ttp.png");
+global.Thumb = fs.readFileSync("./Assets/ttp.png");
+global.Thumb1 = fs.readFileSync("./Assets/ttp.png");
+global.ErrorPic = fs.readFileSync("./Assets/ttp.png");
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+//
+global.mess = {
+    jobdone: 'Here you go...',
+    useradmin: 'Sorry, only *Group Admins* can use this command *Bro*!',
+    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+    botowner: 'Only my *Owner* can use this command, Bro!',
+    grouponly: 'This command is only made for *Groups*, Bro!',
+    privateonly: 'This command is only made for *Private Chat*, Bro!',
+    botonly: 'Only the *Bot itself* can use this command!',
+    waiting: 'Just Wait...',
+    nolink: 'Please provide me *link*, Bro!',
+    error: 'An error occurd!',
+    banned: 'You are *Banned* fron using commands!',
+    bangc: 'This Group is *Banned* from using Commands!',
+    nonsfw: 'Dont be a pervert Bro! This is not a NSFW enabled group!'
+    
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
